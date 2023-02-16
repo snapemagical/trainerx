ef2174e373a65a0fa9652082053016c01d2cf456
pixelxpr <118562810+pixelxpr@users.noreply.github.com>
2023-02-16 10:49:11 +0000

    refactor: page not found

--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-import reportWebVitals from './reportWebVitals'
-import AppContext from './context/appContext'
-import { BrowserRouter } from 'react-router-dom'
-ReactDOM.render(
-    <BrowserRouter>
-    <AppContext>
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+// import AppContext from "./context/appContext";
+
+import "./index.css";
+
+const container = document.getElementById("root");
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(
+  <BrowserRouter>
+    {/* <AppContext> */}
+    <div id="app">
       <App />
-    </AppContext>
-  </BrowserRouter>,
-  document.getElementById('root'),
-)
+    </div>
+    {/* </AppContext> */}
+  </BrowserRouter>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+reportWebVitals();
